b59bc98265f9a73817b109f1d364d88c402c7cfb
argab <argabid@gmail.com>
2022-09-29 22:24:53 +0600

    3.1.1

--- config/index.ts
+++ config/index.ts
@@ -9,7 +9,9 @@ export = <nodart.app.AppConfigInterface>Object.freeze({
      * See session supporting docs: https://github.com/mozilla/node-client-sessions
      */
     session: {
-        duration: 2 * 60 * 60 * 1000
+        secret: 'MY_SUPER_STRONG_SECRET_KEY', // (!!!) CHANGE THIS OPTION IN PRODUCTION ENVIRONMENT (should be a large unguessable string)
+        duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+        activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
     },
     /**
      * See ORM supporting docs: https://knexjs.org/guide/
