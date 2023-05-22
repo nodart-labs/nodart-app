a4c15e968458b78643957e8066eb56b3365f4c6f
argab <argabid@gmail.com>
2023-05-22 17:15:42 +0600

    5.5.0

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -6,51 +6,49 @@ import { nodart } from "nodart";
  The object entry key serves as the name/path to the filename of the controller
  (without "_controller" suffix name and extension) that starts the underlying http processing.
 
- There are 2 ways to pass a route:
+ There are 2 ways to describe a route:
  1. As a string with parameters
- 2. As an array of objects - descriptors or strings
+ 2. As an array of descriptors (objects) or strings
 
  Examples:
 
  1. Passing a route as a string (URL pathname):
 
- export = {
-   sample: 'route/to/sample/controller/:someArgumentNameAndValue/:+id?'
- }
+    export = {
+      controller_name: "route/to/sample/controller/:parameter/:+id?",
+      "path/to/controller_name:controllerAction": "@get:@post:path/:parameter",
+      "path/to/controller_name": [
+          "/route/one",
+          "/route/two",
+      ],
+    }
 
- :+id? - This parameter passes the number "id" to the controller's action as an argument.
-
- Argument pointers:
- : - defines the argument
- + - converts value to a number
- ? - describes optional argument (means that it could be existed or not)
+ Parameter pointers:
+   : - defines the parameter
+   + - converts value to a number
+   ? - describes optional parameter (means that it could be existed or not)
 
  2. Passing a route as an array of objects - descriptors or strings
 
  The descriptor object allows you to specify the name of the route,
  the controller class and its action method,
- and the regular expression or type and function that would be applied to the argument value.
+ and the regular expression or type and function that would be applied to the :parameter value.
 
- export = {
-  sample: [
-     'route/to/sample/controller/:someArgumentNameAndValue/:+id?',
-     {
-         path: string,
-         name?: string,
-         action?: string,
-         controller?: (route: RouteData) => typeof Controller,
-         types?: {
-             [paramName: string]: typeof Number | RegExp | ((value: any) => any)
-         },
-     }
-  ]
-}
+   export = {
+      sample: [
+         "route/to/sample/controller/:parameter/:+id?",
+         {
+             path: string,
+             name?: string,
+             action?: string,
+             controller?: (route: RouteData) => typeof Controller,
+             types?: {
+                 [paramName: string]: typeof Number | RegExp | ((value: any) => any)
+             },
+         }
+      ]
+   }
 
- ---------------------------------------------------------------------------------------------------------
-
- Additionally, you don't need to specify the controller's HTTP route in the setup; you can directly call it.
-
- Just call: http://url/controller_name/action and You will get it.
 
  ***********************************************************************************************************/
 
